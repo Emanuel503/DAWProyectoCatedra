66dec0065b4676eace23f8edccda80c9bf267e36
Emanuel Molina <emanueljosemolina@hotmail.es>
2021-11-04 15:25:58 -0600

    Update departamentos.js

--- js/departamentos.js
+++ js/departamentos.js
@@ -2,18 +2,18 @@ window.onload = cargarDepartamentos();
 
 function cargarDepartamentos(){
     
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
+    $.ajax({
+        url: '../js/departamentos.json ',
+        method: 'GET'
+    }).then(function(data) {
+
+        data.forEach(function(item) {
+            
+
+            alert(item.nombre);
+
+        });
         
-        if(this.readyState == 4 && this.status == 200){
-
-            let departamento = JSON.parse(xhttp.responseText);
-
-            document.getElementById('nombreDepartamento').innerHTML=departamento[0].nombre;
-            document.getElementById('imagenMapa').innerHTML='<img src="../img/"'+departamento[0].mapa+'">';
-
-        }
-    };
-    xhttp.open('GET', '../js/departamentos.json', 'true');
-    xhttp.send();
+    });
+    
 }
