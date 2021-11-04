f66dd8f4c4204b68dea939938d619e0d6da965f7
Emanuel Molina <emanueljosemolina@hotmail.es>
2021-11-04 15:43:54 -0600

    Update departamentos.js

--- js/departamentos.js
+++ js/departamentos.js
@@ -9,9 +9,17 @@ function cargarDepartamentos(){
 
         var nombre = document.getElementById('nombreDepartamento');
         var mapa = document.getElementById('imagenMapa');
+        var datosHistoricos = document.getElementById('datosHistoricos');
+        var datosGenerales = document.getElementById('datosGenerales');
+        var historia = document.getElementById('historia');
+        var municipios = document.getElementById('municipios');
+        var cultura = document.getElementById('cultura');
+        var turismo = document.getElementById('turismo');
+        var tradiciones = document.getElementById('tradiciones');
 
         nombre.innerHTML=departamento[0].nombre;
         mapa.innerHTML='<img src="../img/'+departamento[0].mapa+'">';
-
+        datosHistoricos.innerHTML=departamento.historia.infoHistoria;
+        datosHistoricos.innerHTML+='<img class="img-fluid" src="../img/'+departamento.historia.imagenesHistoria+'">';
     });  
 }
