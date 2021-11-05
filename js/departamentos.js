19d55c057cf1491310c2897a57f27fb64127871f
Emanuel Molina <emanueljosemolina@hotmail.es>
2021-11-04 18:56:53 -0600

    eventos

--- js/departamentos.js
+++ js/departamentos.js
@@ -20,8 +20,47 @@ function cargarDepartamentos(){
         localStorage.setItem("id", "3");
     });
 
-    
-    
+    $( "#la-libertad" ).on( "click", function() {
+        localStorage.setItem("id", "4");
+    });
+
+    $( "#san-salvador" ).on( "click", function() {
+        localStorage.setItem("id", "5");
+    });
+
+    $( "#cuscatlan" ).on( "click", function() {
+        localStorage.setItem("id", "6");
+    });
+
+    $( "#la-paz" ).on( "click", function() {
+        localStorage.setItem("id", "7");
+    });
+
+    $( "#cabanas" ).on( "click", function() {
+        localStorage.setItem("id", "8");
+    });
+
+    $( "#san-vicente" ).on( "click", function() {
+        localStorage.setItem("id", "9");
+    });
+
+    $( "#usulutan" ).on( "click", function() {
+        localStorage.setItem("id", "10");
+    });
+
+    $( "#san-miguel" ).on( "click", function() {
+        localStorage.setItem("id", "11");
+    });
+
+    $( "#morazan" ).on( "click", function() {
+        localStorage.setItem("id", "12");
+    });
+
+    $( "#la-union" ).on( "click", function() {
+        localStorage.setItem("id", "13");
+    });
+
+
     $.ajax({
         url: '../js/departamentos.json',
         method: 'GET'
@@ -37,8 +76,8 @@ function cargarDepartamentos(){
         var tradiciones = document.getElementById('tradiciones');
 
         //Nombre del departamento y mapa
-        nombre.innerHTML=departamento[0].nombre;
-        mapa.innerHTML='<img src="../img/'+departamento[0].mapa+'">';
+        nombre.innerHTML=departamento[id].nombre;
+        mapa.innerHTML='<img src="../img/'+departamento[id].mapa+'">';
 
         //Datos Generales
         datosGenerales.innerHTML+='<b>Nombre: </b>'+departamento[0].nombre+'<br>';
