6d18011ca293bf61bddf9316438337006e3e442f
Emanuel Molina <emanueljosemolina@hotmail.es>
2021-11-09 12:36:11 -0600

    Contactanos

--- js/formulario.js
+++ js/formulario.js
@@ -1,7 +1,11 @@
 //Arreglo donde se guardara el objetoComentario
 var Arraycomentarios = new Array();
 
+//Variable que guarda del localstorage el id del departamento al que se le dio click
 var id=localStorage.getItem("id");
+
+//Varible que guarda el numero de fila para editar o borrar el registro
+var fila;
 
 //Obtencion del los elementos del html, guardandoles en su respectiva variable
 var nombre = document.getElementById('nombre');
@@ -27,7 +31,7 @@ if(window.addEventListener){
 //Funcion para agregar los evento al boton
 function AgregarEventos(){
 
-  btnEnviar.addEventListener('click', comprobarDatos, false);
+  btnEnviar.addEventListener('click', guardarDatos, false);
 
   $( "#santa-ana" ).on( "click", function() {
       localStorage.setItem("id", "0");
@@ -86,6 +90,34 @@ function AgregarEventos(){
   });
 }
 
+//Funcion que guarda los datos en el objeto y luego lo guarda en un arreglo para almaceralo en un localStorage
+function guardarDatos(){
+
+  if(comprobarDatos()==true){
+
+    //Objeto Comentario el cual tendra las propiedades de los compos del fomulario de contacto
+    var objComentario = new Object();
+    //Guarda los datos en su propiedad correspondiente del objeto
+    objComentario.nombre = nombre.value;
+    objComentario.email = email.value;
+    objComentario.pais = pais.value;
+    objComentario.comentario = comentario.value;
+
+    nombre.value="";
+    email.value="";
+    pais.value="";
+    comentario.value="";
+
+    //Guarda el objeto en el arreglo de datos
+    Arraycomentarios.push(objComentario);
+
+    //Guarda el arreglo en el localstorage
+    localStorage.setItem('localComentarios', JSON.stringify(Arraycomentarios));
+
+    mostrarComentarios();
+  }
+}
+
 //Funcion para comprobar y validar los datos introducidos en el fomulario
 function comprobarDatos(){
 
@@ -99,11 +131,11 @@ function comprobarDatos(){
   if(!nombre.value){
     nombre.className = "form-control is-invalid";
     errorNombre.innerHTML = "Ingrese el nombre";
-    return;
+    return(false);
   }else if(expText.test(nombre.value)){
     nombre.className = "form-control is-invalid";
     errorNombre.innerHTML = "Nombre no valido";
-    return;
+    return(false);
   }else{
     nombre.className = "form-control";
   }
@@ -111,11 +143,11 @@ function comprobarDatos(){
   if(!email.value){
     email.className = "form-control is-invalid";
     errorEmail.innerHTML = "Ingrese el email";
-    return;
+    return(false);
   }else if(!expEmail.test(email.value)){
     email.className = "form-control is-invalid";
     errorEmail.innerHTML = "Email no valido";
-    return;
+    return(false);
   }else{
     email.className = "form-control";
   }
@@ -123,11 +155,11 @@ function comprobarDatos(){
   if(!pais.value){
     pais.className = "form-control is-invalid";
     errorPais.innerHTML = "Ingrese el país";
-    return;
+    return(false);
   }else if(expText.test(pais.value)){
     pais.className = "form-control is-invalid";
     errorPais.innerHTML = "Pais no valido";
-    return;
+    return(false);
   }else{
     pais.className = "form-control";
   }
@@ -135,43 +167,17 @@ function comprobarDatos(){
   if(!comentario.value){
     comentario.className = "form-control is-invalid";
     errorComentario.innerHTML = "Ingrese el comentario";
-    return;
+    return(false);
   }else if(expComen.test(comentario.textContent)){
     comentario.className = "form-control is-invalid";
     errorComentario.innerHTML = "Comentario no valido";
-    return;
+    return(false);
   }else{
     comentario.className = "form-control";
   }
 
-  //Si los datos son validos se gurdaran los datos
-  guardarDatos();
+  return(true);
 
-  nombre.value="";
-  email.value="";
-  pais.value="";
-  comentario.value="";
-
-}
-
-//Funcion que guarda los datos en el objeto y luego lo guarda en un arreglo para almaceralo en un localStorage
-function guardarDatos(){
-
-  //Objeto Comentario el cual tendra las propiedades de los compos del fomulario de contacto
-  var objComentario = new Object();
-  //Guarda los datos en su propiedad correspondiente del objeto
-  objComentario.nombre = nombre.value;
-  objComentario.email = email.value;
-  objComentario.pais = pais.value;
-  objComentario.comentario = comentario.value;
-
-  //Guarda el objeto en el arreglo de datos
-  Arraycomentarios.push(objComentario);
-
-  //Guarda el arreglo en el localstorage
-  localStorage.setItem('localComentarios', JSON.stringify(Arraycomentarios));
-
-  mostrarComentarios();
 }
 
 //Funcion que muesta los comentarios que han sido registrados, que estan almacenados en el localStorage
@@ -193,15 +199,18 @@ function mostrarComentarios(){
   tabla+='<table class="table table-bordered table-striped" id="tableDatos">';
   tabla+='<thead>';
   tabla+='<tr>';
+  tabla+='<th>ID</th>';
   tabla+='<th>Datos personales</th>';
   tabla+='<th>Comentario</th>';
-  tabla+='<th>Editar</th>';
-  tabla+='<th>Eliminar</th>';
+  tabla+='<th>Opciones</th>';
   tabla+='</tr>';
   tabla+='</thead>';
   tabla+='<tbody>';
   for (var i=0; i<Arraycomentarios.length;i++){
     tabla+='<tr>';
+      tabla+='<td>'
+      tabla+=(i+1);
+      tabla+='</td>'
       tabla+='<td>'
       tabla+='Nombre: '+Arraycomentarios[i].nombre+'<br>';
       tabla+='Email: '+Arraycomentarios[i].email+'<br>';
@@ -211,10 +220,8 @@ function mostrarComentarios(){
       tabla+=Arraycomentarios[i].comentario;
       tabla+='</td>';
       tabla+='<td>'
-      tabla+='<img class="botones" id="'+i+'" src="../img/arrow-clockwise.svg" alt="Editar">';
-      tabla+='</td>';
-      tabla+='<td>'
-      tabla+='<img class="botones" id="0'+i+'" src="../img/x-square-fill.svg" alt="Eliminar">';
+      tabla+='<center><button type="button" data-toggle="modal" data-target="#exampleModal" class="btn btn-success" id="editar">Editar</button>   ';
+      tabla+='<button class="btn btn-danger" id="eliminar">Eliminar</button></center>';
       tabla+='</td>';
     tabla+='<tr>';
   }
@@ -224,29 +231,65 @@ function mostrarComentarios(){
   //Inserta la taba en html
   comentariosRegistrados.innerHTML = tabla;
 
-   //Obtiene el elemento del html
-  for (var i=0; i<Arraycomentarios.length;i++){
-    var editar = document.getElementById(i);
-    var eliminar = document.getElementById("0"+i);
-
-    //agrega un evento click a las imagenes
-    editar.addEventListener('click', editarComentario, false);
-    eliminar.addEventListener('click', eliminarComentario, false);
-  }
 }
 
-//Funcion que permite editar el comenatario 
-function editarComentario(){
+//Editar comentario
+$(document).on("click", ".btn-success", function(){
 
-  nombre.value = Arraycomentarios[0].nombre;
-  email.value = Arraycomentarios[0].email;
-  pais.value = Arraycomentarios[0].pais;
-  comentario.value = Arraycomentarios[0].comentario;
+  fila = $(this).closest("tr");
+  id = (parseInt(fila.find('td:eq(0)').text()));
+  
+  id=id-1;
 
-}
+  nombre.value=Arraycomentarios[id].nombre;
+  email.value=Arraycomentarios[id].email;
+  pais.value=Arraycomentarios[id].pais;
+  comentario.value=Arraycomentarios[id].comentario;
 
-//Funcion que elmina el comentario
-function eliminarComentario(){
-  localStorage.clear();
+  comprobarDatos();
+
+  $("#btnEnviar").css('display', 'none');
+  $("#btnGuardar").css('display', 'inline-block');
+  $("#btnCancelar").css('display', 'inline-block');
+
+  localStorage.setItem('idComentarioEditar', id);
+
+});
+
+$( "#btnGuardar" ).on( "click", function(){
+
+  idComentarioEditar = localStorage.getItem("idComentarioEditar");
+  Arraycomentarios[idComentarioEditar].nombre = nombre.value;
+  Arraycomentarios[idComentarioEditar].email = email.value;
+  Arraycomentarios[idComentarioEditar].pais = pais.value;
+  Arraycomentarios[idComentarioEditar].comentario = comentario.value;
+
+  localStorage.setItem('localComentarios', JSON.stringify(Arraycomentarios));
+
   mostrarComentarios();
-}
+
+  $("#btnEnviar").css('display', 'inline-block');
+  $("#btnGuardar").css('display', 'none');
+  $("#btnCancelar").css('display', 'none');
+
+  nombre.value="";
+  email.value="";
+  pais.value="";
+  comentario.value="";
+
+});
+
+//Eliminar comentario
+$(document).on("click", ".btn-danger", function(){
+
+  fila = $(this).closest("tr");
+  id = (parseInt(fila.find('td:eq(0)').text()));
+  
+  id=id-1;
+
+  Arraycomentarios.splice(id);
+
+  localStorage.setItem('localComentarios', JSON.stringify(Arraycomentarios));
+  
+  mostrarComentarios();
+});
